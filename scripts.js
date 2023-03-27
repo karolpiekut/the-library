687b2e533468ac2a6ff46b4150942b7e2cdd1fdc
kpiekut <karol.piekut@pm.me>
2023-03-27 14:49:57 +0100

    fix number input

--- scripts.js
+++ scripts.js
@@ -1,66 +1,61 @@
+const theLibraryTable = document.querySelector("#library-items");
+const dataEntry = document.querySelector('#library-input');
+
 let library = [
 ];
-
-/*
-1. get objects to show as html table items
-2. get event listeners sorted
-3. get form so create object items
-4. object functions
- */
-
-function Book(id, title, author, pages, readStatus) {
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.readStatus = readStatus;
 }
 
+function addNewBook(event) {
+    event.preventDefault();
+    const bookTitle = (this.querySelector('[name=title]')).value;
+    const bookAuthor =  (this.querySelector('[name=author]')).value;
+    const bookPages = (this.querySelector('[name=pages]')).value;
 
-
-const theLibraryTable = document.querySelector("#library-items");
-const dataEntry = document.querySelector('#data-entry');
-
-function createTableRow() {
-    const newNode = document.createElement("tr");
-    const newTDid = newNode.appendChild(document.createElement("td"));
-    const newTDTitle = newNode.appendChild(document.createElement("td"));
-    const newTDAuthor = newNode.appendChild(document.createElement("td"));
-    const newTDPages = newNode.appendChild(document.createElement("td"));
-    const newTDStatus = newNode.appendChild(document.createElement("td"));
-    const newTDRemove = newNode.appendChild(document.createElement("td"));
-    let objectID = document.createTextNode(library[0].id);
-    let objectTitle = document.createTextNode(library[0].title);
-    let objectAuthor = document.createTextNode(library[0].author);
-    let objectPages = document.createTextNode(library[0].pages);
-    let objectStatus = document.createTextNode(library[0].readStatus);
-
-
-    newTDid.appendChild(objectID);
-    newTDTitle.appendChild(objectTitle);
-    newTDAuthor.appendChild(objectAuthor);
-    newTDPages.appendChild(objectPages);
-    newTDStatus.appendChild(objectStatus);
-    theLibraryTable.appendChild(newNode);
-}
-
-function addNewBook(e) {
-    e.preventDefault()
-    const newBook = new Book(
-        this.querySelector('[name=title]').value,
-        this.querySelector('[name=author]').value,
-        this.querySelector('[name=pages]').value,
-        this.querySelector('[name=read-status]').value
-        )
-    library.push(newBook);
-    //this.reset();
+    if (bookTitle === '' || bookAuthor === '' || bookPages === '') {
+        alert("Please populate all input fields.")
+    } else {
+        const newBook = new Book(bookTitle, bookAuthor, bookPages);
+        library.push(newBook);
+        populateTable(library, theLibraryTable);
+        this.reset();
+    }
     console.table(library);
 }
 
-/*
-function populateTable() {
+function populateTable(library = [], theLibraryTable) {
+    theLibraryTable.innerHTML += library.map((book, i) => {
+        return `
+            <tr>
+            <td class="table-item">1</td>
+            <td class="table-item left">${library[i].title}</td>
+            <td class="table-item left">${library[i].author}</td>
+            <td class="table-item">${library[i].pages}</td>
+            <td class="table-item">Read</td>
+            <td class="table-item"><img class="delete" src="./trash-can-outline.svg" alt="delete-icon"></td>
+            </tr>
+        `;
+    }).join('');
 
+}
+/*
+    for (let i = 0; i < library.length; i++) {
+        theLibraryTable.innerHTML = `
+            <td class="table-item">1</td>
+            <td class="table-item left">${library[i].title}</td>
+            <td class="table-item left">${library[i].author}</td>
+            <td class="table-item">${library[i].pages}</td>
+            <td class="table-item">Read</td>
+            <td class="table-item"><img class="delete" src="./trash-can-outline.svg" alt="delete-icon"></td>
+        `;
+    }/*
 }
 */
 
-
 dataEntry.addEventListener('submit', addNewBook);
+
+
+
