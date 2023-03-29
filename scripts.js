228d371493d59022433598e8e38c9a3d5ee1c03c
kpiekut <karol.piekut@pm.me>
2023-03-29 14:29:58 +0100

    fix add new items

--- scripts.js
+++ scripts.js
@@ -30,8 +30,16 @@ function addNewBook(event) {
 }
 
 function populateTable(library = [], theLibraryTable) {
-    theLibraryTable.innerHTML += library.map((book, i) => {
-        return `
+    let str = `<tr id="header">
+            <th class="header-item" id="id-header">ID</th>
+            <th class="header-item left" id="title-header">Title</th>
+            <th class="header-item left" id="author-header">Author</th>
+            <th class="header-item" id="pages-header">Pages</th>
+            <th class="header-item" id="read-status-header">Status</th>
+            <th class="header-item" id="remove-header">Remove</th>
+        </tr>`
+    for (let i = 0; i < library.length; i++) {
+        str +=`
             <tr>
             <td class="table-item">1</td>
             <td class="table-item left">${library[i].title}</td>
@@ -41,24 +49,10 @@ function populateTable(library = [], theLibraryTable) {
             <td class="table-item"><img class="delete" src="./trash-can-outline.svg" alt="delete-icon"></td>
             </tr>
         `;
-    }).join('');
+    }
+    theLibraryTable.innerHTML = str;
+}
 
-}
-/*
-    for (let i = 0; i < library.length; i++) {
-        theLibraryTable.innerHTML = `
-            <td class="table-item">1</td>
-            <td class="table-item left">${library[i].title}</td>
-            <td class="table-item left">${library[i].author}</td>
-            <td class="table-item">${library[i].pages}</td>
-            <td class="table-item">Read</td>
-            <td class="table-item"><img class="delete" src="./trash-can-outline.svg" alt="delete-icon"></td>
-        `;
-    }/*
-}
-*/
 
 dataEntry.addEventListener('submit', addNewBook);
-
-
 
